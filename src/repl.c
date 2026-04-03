ff784698252730fc33d4a962da69b5e432fe56a2
Jonathan Y <jonathanyohannes421@gmail.com>
2026-04-04 00:03:04 +0100

    feat: Moved function suitable for executing to appropate file, updated to handle the call for system level functions

--- src/repl.c
+++ src/repl.c
@@ -1,8 +1,5 @@
 #include "shell.h"
 
-#define TOK_BUFSIZE 64
-#define TOK_DELIM " \t\r\n\a"
-
 void run_shell_loop(void) 
 {
     char *line;
@@ -20,69 +17,5 @@ void run_shell_loop(void)
         free(line);
         free(args);
     }while(status);
-}
-
-char *read_line(void)
-{
- char *line = NULL;
- size_t bufsize = 0;
-
- if (getline(&line, &bufsize, stdin) == -1)
- {
-	// Check if we meet EOL
-	if(feof(stdin))
-	{
-		free(line);
-		exit(EXIT_SUCCESS);
-	}
-	else
- 	{
-		free(line); /* avoid memory leaks when getline fails */
-		perror("error while reading the line from stdin");
-   		exit(EXIT_FAILURE);
-  	}
- }
- return (line);
-}
-
-char **read_args(char *line) 
-{
-    int bufsize = TOK_BUFSIZE;
-    int position = 0;
-    char **tokens = malloc(bufsize * sizeof(char*));
-    char *token;
-
-    if (!tokens) 
-    {
-        fprintf(stderr, "yoshell: allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // strtok returns a pointer to the first "token" (word) it finds
-    token = strtok(line, TOK_DELIM);
-    while (token != NULL)
-    {
-        tokens[position] = token;
-        position++;
-
-        // If we run out of space in our array, reallocate more
-        if (position >= bufsize) 
-	{
-            bufsize += TOK_BUFSIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if (!tokens) 
-	    {
-                fprintf(stderr, "yoshell: allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-
-        // Subsequent calls to strtok must use NULL as the first argument
-        token = strtok(NULL, TOK_DELIM);
-    }
-    
-    // The "NULL-terminated" requirement for execvp
-    tokens[position] = NULL;
-    return tokens;
 }
 
