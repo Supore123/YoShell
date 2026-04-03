655b6069c5e2c0573006591a7eb1370dc95dfd90
Jonathan Y <jonathanyohannes421@gmail.com>
2026-04-03 19:36:52 +0100

    feat: updated handling of the reading of lines and echos output

--- src/repl.c
+++ src/repl.c
@@ -3,17 +3,43 @@
 void run_shell_loop(void) 
 {
     char *line;
-    char **args;
-    int status;
+    //char **args;
+    int status = 1;
 
     do
     {
         printf("yoshell> ");
-        line = read_line();
-        args = read_args();
-        status =  execute_args();       
 
+        line = read_line(); 		// Get the string
+        args = read_args(); 		// Break into parsing 
+//       status =  execute_args(); 	// Execute command
+
+	// Temp test
+	printf("%s", line);
         free(line);
         free(args);
     }while(status);
+}
+
+char *read_line(void)
+{
+ char *line = NULL;
+ size_t bufsize = 0;
+
+ if (getline(&line, &bufsize, stdin) == -1)
+ {
+	// Check if we meet EOL
+	if(feof(stdin))
+	{
+		free(line);
+		exit(EXIT_SUCCESS);
+	}
+	else
+ 	{
+		free(line); /* avoid memory leaks when getline fails */
+		perror("error while reading the line from stdin");
+   		exit(EXIT_FAILURE);
+  	}
+ }
+ return (line);
 }
