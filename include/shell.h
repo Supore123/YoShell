468e2f17f7a55c2c5e71e00e5dc1c04769790c01
Jonathan Y <jonathanyohannes421@gmail.com>
2026-04-04 00:03:34 +0100

    feat: update includes for function protypes for custom built-ins

--- include/shell.h
+++ include/shell.h
@@ -4,12 +4,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h> // For ssize_t
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-// Function prototypes
+// Function Defines
 void run_shell_loop(void);
 char *read_line(void);
 char **read_args(char *line);
 int execute_args(char **args);
+int launch_process(char **args); // New helper for external commands
+
+// Custom built in shell handlers
+int shell_cd(char **args);
+int shell_exit(char **args);
+int shell_help(char **args);
+int num_builtins();
+
+// Declarations to make arrays visible across files
+extern char *builtin_str[];
+extern int (*builtin_func[]) (char **);
 
 #endif
